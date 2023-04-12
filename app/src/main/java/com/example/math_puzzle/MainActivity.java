cc9d4a26fd73f0acf432fa1ad0fb3d3554e2eb06
Shayri <viranipradip100@gmail.com>
2023-04-12 11:16:37 +0530

    3

--- app/src/main/java/com/example/math_puzzle/MainActivity.java
+++ app/src/main/java/com/example/math_puzzle/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.math_puzzle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+TextView textView1,textView2,textView3,textView4;
+Button button1,button2;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView1=findViewById(R.id.continue_txt);
+        textView2=findViewById(R.id.puzzle_txt);
+        textView3=findViewById(R.id.buy_txt);
+        textView4=findViewById(R.id.privete_txt);
+        button1=findViewById(R.id.share_button);
+        button2=findViewById(R.id.mail_button);
+
     }
 }
